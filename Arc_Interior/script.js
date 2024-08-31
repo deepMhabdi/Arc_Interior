e337871345f43561658a355000e9a4126d8312f4
Deep Mhabdi <dmmhabdi2701@gmail.com>
2024-08-31 18:08:37 +0530

    first commit

---
+++ Arc_Interior/script.js
@@ -0,0 +1,5 @@
+// Navbar Toggle
+document.getElementById('navbar-toggle').addEventListener('click', function () {
+    var menu = document.getElementById('navbar-menu');
+    menu.classList.toggle('hidden');
+});
